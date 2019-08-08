e380a9de290045a25e4b175c95c3c1f0674e3f90
yixiu88 <yixiu88@outlook.com>
2019-08-08 17:41:47 +0800

    update

--- docs/.vuepress/theme/components/page.vue
+++ docs/.vuepress/theme/components/page.vue
@@ -1,56 +1,31 @@
 <template>
   <div class="component-page">
     <home v-if="$route.path === '/'"></home>
-    <div
-      v-else
-      class="page-content">
+    <div v-else class="page-content">
       <item-list v-if="isBlog"></item-list>
-      <Content/>
+      <Content />
     </div>
-    <el-button
-      v-if="isResume"
-      type="primary"
-      icon="el-icon-download"
-      class="download-btn"
-      @click="downloadResume">
-      下载简历
-    </el-button>
   </div>
 </template>
 
 <script>
-import html2canvas from 'html2canvas'
 import Home from './Home.vue'
 import ItemList from './item-list.vue'
 import 'prismjs/themes/prism-tomorrow.css'
 import 'prismjs/prism.js'
-
 function downLoadImage(canvas,name) {
     var a = document.createElement("a")
     a.href = canvas.toDataURL()
     a.download = name
     a.click()
 }
-
 export default {
   computed: {
     isBlog () {
       return this.$route.path === '/blog/'
     },
-
-    isResume () {
-      return this.$route.path === '/my/'
-    }
   },
-
-  methods: {
-    downloadResume () {
-      html2canvas(document.querySelector('#resume')).then(canvas => {
-        downLoadImage(canvas, '简历')
-      })
-    }
-  },
-
+  methods: {},
   components: {
     Home,
     ItemList
@@ -63,12 +38,12 @@ export default {
   width: 100%;
   height: 100%;
   overflow: auto;
-  font-family: -apple-system,BlinkMacSystemFont,Helvetica Neue,PingFang SC,Microsoft YaHei,Source Han Sans SC,Noto Sans CJK SC,WenQuanYi Micro Hei,sans-serif;
-
+  font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue, PingFang SC,
+    Microsoft YaHei, Source Han Sans SC, Noto Sans CJK SC, WenQuanYi Micro Hei,
+    sans-serif;
   .page-content {
     width: 800px;
     margin: 15px auto;
-
     .custom {
       img {
         max-width: 800px;
@@ -76,7 +51,6 @@ export default {
       }
     }
   }
-
   .download-btn {
     position: fixed;
     top: 100px;
